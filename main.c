496923e8bb5d8d3b75952e172ae4b2a87f204ec0
Xpirr <evrenn.ispiroglu@gmail.com>
2021-06-17 21:04:01 +0300

    Lexicographic rank v2 Now storing the repetition counts in an array and printing the time ranks.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void entering_word (char *string)
 }
 void printing_word (char *string)
 {
-    printf("You have entered word %s \n",string);
+    printf("You have entered word ` %s ` \n",string);
 }
 
 unsigned long long fac(unsigned long long n)
@@ -39,17 +39,15 @@ unsigned long long greater_than(char *string, unsigned long long n, unsigned lon
     return count;
 }
 
-unsigned long long repetition(char *string, unsigned long long len, unsigned long long n)
+void determining_repetition(unsigned long long *repetition, char *string, unsigned long long len)
 {
-
-    unsigned long long count, repetition = 1;
     char repeats[30];
+    unsigned long long sum = 0;
+
     int j = 0, m = 0, flag;
 
-    for (unsigned long long i = n; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        count = 0; flag = 0;
-
         while (j < m && flag == 0) {
             if (repeats[j] == string[i])
             {
@@ -58,42 +56,57 @@ unsigned long long repetition(char *string, unsigned long long len, unsigned lon
             j++;
         }
         j = 0;
-        if (flag == 0) {
 
-            for (unsigned long long k = n; k < len ;k++)
+        if (flag == 0)
+        {
+            for (int k = i; k < len; k++)
             {
-                if (string[i] == string[k])
+                if(string[i] == string[k])
                 {
+                    repetition[i]++;
                     repeats[m] = string[i];
                     m++;
-                    count++;
                 }
             }
         }
-
-        repetition *= fac(count);
     }
 
-    return repetition;
-
+    for (int i = 0; i < m; i++)
+    {
+        sum = 0;
+        for (int k = i; k < m; k++)
+        {
+            if (repetition[k] > 1)
+            {
+                sum += repetition[k];
+            }
+        }
+        if (sum != 0)
+            repetition[i] = sum;
+    }
 }
+
+
 
 unsigned long long ranking(char *string)
 {
 
-
-
     unsigned long long len = strlen(string);
     unsigned long long rank = 1, count, rep;
+
+    unsigned long long repetition[52];
+    determining_repetition(repetition, string, len);
 
     for (unsigned long long i = 0; i < len; i++)
     {
         unsigned long long temp =  1;
 
         count = greater_than(string, i, len - 1);
+
         temp *=  count * fac(len - i - 1);
-        rep = repetition(string, len, i);
-        temp /= rep;
+
+        if (repetition[i] != 0)
+            temp /= repetition[i];
 
         rank += temp;
     }
@@ -107,31 +120,55 @@ void lexicographic_rank (double *times, int count)
 
 
     entering_word(string);
-    printing_word(string);
 
-    clock_t begin = clock();
+    clock_t begin = clock();    // From this line algorithm is executing.
 
     rank = ranking(string);
-    for (int i = 0; i < 99999; i++)
+    for (int i = 0; i < 999999; i++)
     {
         ranking(string);
     }
 
-    clock_t end = clock();
+    clock_t end = clock();      // From this line algorithm has stopped.
 
-    printf("\n");
+    printf("\n\n---------------------------------------------------------------------------------------------------\n");
+    printing_word(string);
     printf("Rank of your word is %llu\n", rank);
+    printf("---------------------------------------------------------------------------------------------------\n");
+    printf("||||||||||||||||||||||||||||||||||\n");
 
 
 
     double time_sp = (double)(end - begin);
-    time_sp /= 100000;
+    time_sp /= 1000000;
     times[count] = time_sp;
 
+    printf("---------------------------------------------------------------------------------------------------\n");
     printf("This program executed for %llu indexed array on average %lf miliseconds.\n", strlen(string), time_sp);
-
+    printf("---------------------------------------------------------------------------------------------------\n\n\n");
 }
 
+graphic(double *times, int count)
+{
+
+    printf("Let us compare the execution times!! \n");
+    printf("---------------------------------------------------------------------------------------------------\n");
+    for (int i = 1; i <= count; i++)
+    {
+        times[i] = times[i] * 100000;
+    }
+
+    for(int i = 1; i <= count; i++)
+    {
+        printf("For word %d |", i);
+        for(int j = 0; j < times[i]; j++)
+        {
+            printf("*");
+        }
+        printf("|\n");
+    }
+
+}
 
 
 int main() {
@@ -141,9 +178,9 @@ int main() {
     do {
         count++;
 
-        printf("---------------------------------\n");
+        printf("---------------------------------------------------------------------------------------------------\n");
         lexicographic_rank(times, count);
-        printf("---------------------------------\n");
+        printf("---------------------------------------------------------------------------------------------------\n");
 
 
         printf(" For exiting the program please enter -1 \n For resuming enter the word. \n");
@@ -151,11 +188,12 @@ int main() {
 
     }while(i != -1);
 
+    printf("\n\n---------------------------------------------------------------------------------------------------\n");
+    graphic(times, count);
+    printf("---------------------------------------------------------------------------------------------------\n");
 
 
-
-
-
+    printf("Thanks for using the program. \n");
 
     return 0;
 }
